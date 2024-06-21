089c2469df822d5546d05373403ae1a518314c89
happy wang <mail@wangwenbo.me>
2024-06-21 18:23:38 +0800

    增加block使用的颜色数组

---
+++ packages/writer/src/components/ColorfulBlock.js
@@ -0,0 +1,107 @@
+export default [
+  {
+    name: 'voilet',
+    variants: {
+      light: {
+        color: '#6E56CF',
+        border: '#D4CAFE',
+        background: '#F4F0FE'
+      },
+      dark: {
+        color: '#6E56CF',
+        border: '#473876',
+        background: '#291F43'
+      }
+    }
+  },
+  {
+    name: 'green',
+    variants: {
+      light: {
+        color: '#30A46C',
+        border: '#ADDDC0',
+        background: '#E6F6EB'
+      },
+      dark: {
+        color: '#30A46C',
+        border: '#20573E',
+        background: '#132D21'
+      }
+    }
+  },
+  {
+    name: 'orange',
+    variants: {
+      light: {
+        color: '#F76B15',
+        border: '#FFC182',
+        background: '#FFEFD6'
+      },
+      dark: {
+        color: '#F76B15',
+        border: '#66350C',
+        background: '#331E0B'
+      }
+    }
+  },
+  {
+    name: 'tomato',
+    variants: {
+      light: {
+        color: '#E54D2E',
+        border: '#FDBDAF',
+        background: '#FEEBE7'
+      },
+      dark: {
+        color: '#E54D2E',
+        border: '#6E2920',
+        background: '#391714'
+      }
+    }
+  },
+  {
+    name: 'indigo',
+    variants: {
+      light: {
+        color: '#3E63DD',
+        border: '#C1D0FF',
+        background: '#EDF2FE'
+      },
+      dark: {
+        color: '#3E63DD',
+        border: '#304384',
+        background: '#182449'
+      }
+    }
+  },
+  {
+    name: 'yellow',
+    variants: {
+      light: {
+        color: '#D5AE39',
+        border: '#F3D768',
+        background: '#FFFAB8'
+      },
+      dark: {
+        color: '#FFE629',
+        border: '#524202',
+        background: '#2D2305'
+      }
+    }
+  },
+  {
+    name: 'gray',
+    variants: {
+      light: {
+        color: '#8D8D8D',
+        border: '#D9D9D9',
+        background: '#F0F0F0'
+      },
+      dark: {
+        color: '#6E6E6E',
+        border: '#3A3A3A',
+        background: '#222222'
+      }
+    }
+  }
+];
