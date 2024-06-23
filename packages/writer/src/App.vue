f82be614f4f5bf617f355b5dbc8a7b41d20545a4
Wenbo Wang <mail@wangwenbo.me>
2024-06-23 22:50:28 +0800

    尝试html中选然代码方案

--- packages/writer/src/App.vue
+++ packages/writer/src/App.vue
@@ -18,16 +18,45 @@
   import { ref, onMounted } from 'vue'
   const renderJSON = ref(null)
   const renderHTML = ref(null)
+  function extractAndReplaceCodeBlocks(html) {
+    // 正则表达式匹配 <pre><code>...</code></pre> 片段
+    const regex = /<pre[^>]*><code[^>]*>([\s\S]*?)<\/code><\/pre>/g;;
+    let matches;
+    // const codeBlocks = [];
 
+    // 使用正则表达式匹配所有符合条件的片段
+    html = html.replace(regex, (match, p1) => {
+      // codeBlocks.push(p1); // 提取代码片段并存入数组
+      console.log(p1)
+      const decodedCode = decodeHtmlEntities(p1.trim());
+      const code = (hljs.highlightAuto(decodedCode).value)
+      console.log(code)
+      return `<pre><code>${code}</code></pre>`; // 可以根据需求进行不同的替换处理
+    });
+
+    // return {
+    //   html,
+    //   codeBlocks
+    // };
+
+    return html
+  }
+  function decodeHtmlEntities(text) {
+    const textArea = document.createElement('textarea');
+    textArea.innerHTML = text;
+    return textArea.value;
+  }
   const onUpdate = ({ json, html }) => {
     renderJSON.value = json
-    renderHTML.value = html
-
+    console.log('aaa')
+    renderHTML.value = extractAndReplaceCodeBlocks(html)
   }
 
-  onMounted(() => {
-    hljs.highlightAll();
-  })
+
+
+  // onMounted(() => {
+  //   hljs.highlightAll();
+  // })
 </script>
 <style>
   html,
