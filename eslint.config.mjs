57ac2790c6db11fa9b553383857624e4f3cfde28
happy wang <mail@wangwenbo.me>
2024-07-15 14:44:11 +0800

    优化eslint配置生效范围

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,9 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginVue from "eslint-plugin-vue";
+import antfu from '@antfu/eslint-config'
 
-
-export default [
-  {files: ["**/*.{js,mjs,cjs,vue}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
-];
+export default antfu({
+  rules: {
+    'vue/block-order': ['error', {
+      order: [['template', 'script'], 'style'],
+    }],
+  },
+})
