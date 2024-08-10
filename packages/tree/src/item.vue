0854a6e9a377c4836c4378ee7edf7528fe8dedbb
Wenbo Wang <mail@wangwenbo.me>
2024-08-10 20:16:38 +0800

    优化定位线思路

--- packages/tree/src/item.vue
+++ packages/tree/src/item.vue
@@ -1,16 +1,15 @@
 <template>
   <div
     draggable="true"
-    class="pr-1 py-1 rounded-lg flex items-center text-sm w-full flex-1 relative"
+    class="pr-1 py-1 rounded-lg flex items-center text-sm w-full flex-1 relative  "
     :class="[
-      false
-        ? 'hover:bg-blue-200 text-blue-700 active:bg-blue-100 bg-blue-200 font-medium'
-        : 'hover:bg-gray-100 active:bg-gray-200',
-      isDragover && itemDirectionIndex === 0 ? 'bg-blue-300 text-blue-800' : '',
+      isDragover && itemDirectionIndex === 0 ? 'bg-blue-50 outline outline-blue-600 text-blue-800' : '',
+      itemStartDrag ? '' : 'hover:bg-gray-100 active:bg-gray-200 ',
     ]"
     :style="{
       paddingLeft: `${levelRef === 1 ? 4 : (levelRef - 1) * 16 + 4}px`,
     }"
+    @drag="itemDrag"
     @dragstart="itemDragstart"
     @dragover="itemDragover"
     @dragleave="itemDragleave"
@@ -19,6 +18,11 @@
     @mouseleave="mouseleave"
     @dragend="itemDragend"
   >
+    <div
+      v-if="itemDirectionIndex === -1"
+      class="helper h-0.5 inset-x-0 absolute bg-blue-600 -top-px pointer-events-none before:content-[' '] before:block before:size-2.5 before:rounded-full before:border-2 before:border-blue-600 before:absolute before:-left-2 before:-top-[3.5px]"
+      :style="{ left: `${levelRef === 1 ? 4 : (levelRef - 1) * 16 + 4}px` }"
+    />
     <IconBtn
       icon="i-ri-arrow-drop-right-line"
       :visibility="data.children.length > 0"
@@ -29,10 +33,10 @@
     <i class="i-ri-file-3-line text-gray-400" />
     <div
       v-if="!itemIsRename"
-      class="pl-1 truncate"
+      class="pl-1 truncate" :class="[itemStartDrag ? 'opacity-20' : 'opacity-100']"
       style="max-width: calc(100% - 3rem)"
     >
-      {{ data.title }}
+      {{ data.title }}{{ itemStartDrag }}
     </div>
     <div class="ml-auto flex items-center space-x-1 pr-1">
       <IconBtn
@@ -41,6 +45,7 @@
         @click.stop.prevent="addItem"
       />
     </div>
+    <div v-if="(data.children.length > 0 || data.toggle === false) && itemDirectionIndex === 1 " class="helper h-0.5 inset-x-0 absolute bg-blue-600 -bottom-px pointer-events-none before:content-[' '] before:block before:size-2.5 before:rounded-full before:border-2 before:border-blue-600 before:absolute before:-left-2 before:-top-[3.5px]" :style="{ left: `${levelRef === 1 ? 4 : (levelRef - 1) * 16 + 4}px` }" />
   </div>
 
   <template v-if="data.toggle">
@@ -49,6 +54,7 @@
       :key="child.id"
       :data="child"
       :tree-level="levelRef"
+      @item-drag="emits('itemDrag')"
       @item-dragstart="emits('itemDragstart')"
       @item-dragover="emits('itemDragover')"
       @item-dragleave="emits('itemDragleave')"
@@ -66,10 +72,10 @@ import IconBtn from './iconBtn.vue'
 const props = defineProps({
   data: { type: Object },
   treeLevel: { type: Number, default: 0 },
-  parentOffsetTop: { type: Number, default: 0 },
 })
 
 const emits = defineEmits([
+  'itemDrag',
   'itemDragstart',
   'itemDragover',
   'itemDragleave',
@@ -84,7 +90,9 @@ const {
   isDragover,
   itemIsRename,
   itemDirectionIndex,
+  itemStartDrag,
   isHover,
+  itemDrag,
   itemDragstart,
   itemDragover,
   itemDragleave,
--- packages/tree/src/useItem.js
+++ packages/tree/src/useItem.js
@@ -1,11 +1,21 @@
 import { computed, ref } from 'vue'
 
+function itemBeDragShadow(text) {
+  const shadowEl = document.createElement('div')
+  shadowEl.textContent = text
+  shadowEl.style.position = 'fixed'
+  shadowEl.setAttribute('class', 'pointer-events-none px-4 py-px bg-white border rounded-lg text-xs shadow-lg hidden min-w-[220px]')
+  document.body.appendChild(shadowEl)
+  return shadowEl
+}
+const sEl = itemBeDragShadow('')
 export function useItem(props, emits) {
   const dragStartItemId = ref(null)
   const dragOverItemId = ref(null)
   const itemDirectionIndex = ref(null)
   const itemIsRename = ref(false)
-  const itemPosHelper = ref(null)
+  // const itemPosHelper = ref(null)
+  const itemStartDrag = ref(0)
   const levelRef = ref(props.treeLevel + 1) // 直接在定义时计算
 
   const isDragover = computed(() => dragOverItemId.value === props.data.id)
@@ -13,36 +23,51 @@ export function useItem(props, emits) {
   const updateDirectionAndPosition = (itemInnerPosY, itemHeight, itemOffsetTop) => {
     if (itemInnerPosY <= itemHeight * 0.25) {
       itemDirectionIndex.value = -1
-      itemPosHelper.value = itemOffsetTop - props.parentOffsetTop
+      // itemPosHelper.value = itemOffsetTop - props.parentOffsetTop
     }
     else if (itemInnerPosY >= itemHeight - itemHeight * 0.25) {
       itemDirectionIndex.value = 1
-      itemPosHelper.value = itemOffsetTop - props.parentOffsetTop + itemHeight
+      // itemPosHelper.value = itemOffsetTop - props.parentOffsetTop + itemHeight
     }
     else {
       itemDirectionIndex.value = 0
-      itemPosHelper.value = null
+      // itemPosHelper.value = null
     }
   }
+  const transparentImage = new Image(0, 0)
+  transparentImage.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
 
   const itemDragstart = (ev) => {
+    ev.dataTransfer.setDragImage(transparentImage, 0, 0)
     ev.dataTransfer.setData('text/plain', JSON.stringify(props.data))
+    sEl.textContent = props.data.title
+    sEl.style.display = 'block'
+    itemStartDrag.value = 1
     dragStartItemId.value = props.data.id
     emits('itemDragstart')
   }
+  const itemDrag = (ev) => {
+    if (ev.pageX === 0 && ev.pageY === 0)
+      return
+    sEl.style.left = `${ev.pageX}px`
+    sEl.style.top = `${ev.pageY}px`
+  }
 
   const itemDragover = (ev) => {
+    ev.stopPropagation()
     ev.preventDefault()
     const itemRect = ev.target.getBoundingClientRect()
     updateDirectionAndPosition(ev.offsetY, ev.target.offsetHeight, itemRect.top)
     dragOverItemId.value = props.data.id
     emits('itemDragover', {
-      helperPosTop: itemPosHelper.value,
-      helperPosLeft: (levelRef.value - 1) * 24,
+      // helperPosTop: itemPosHelper.value,
+      // helperPosLeft: (levelRef.value - 1) * 24,
     })
   }
 
   const itemDragleave = (ev) => {
+    itemDirectionIndex.value = null
+
     ev.preventDefault()
     emits('itemDragleave')
   }
@@ -57,9 +82,18 @@ export function useItem(props, emits) {
         dir: itemDirectionIndex.value,
       })
     }
+    sEl.style.display = 'none'
+    itemStartDrag.value = 0
     dragOverItemId.value = null
     dragStartItemId.value = null
+    itemDirectionIndex.value = null
   }
+  document.body.addEventListener('drop', (ev) => {
+    ev.preventDefault()
+    itemStartDrag.value = 0
+    sEl.style.display = 'none'
+    console.log('up')
+  }, false)
 
   const childrenDrop = ({ from, to, dir }) => emits('itemDrop', { from, to, dir })
 
@@ -93,7 +127,9 @@ export function useItem(props, emits) {
     isDragover,
     itemIsRename,
     itemDirectionIndex,
+    itemStartDrag,
     isHover,
+    itemDrag,
     itemDragstart,
     itemDragover,
     itemDragleave,
