1664a96d554fb52aef50f6773f03be028cc7a55f
Wenbo Wang <mail@wangwenbo.me>
2024-08-25 00:19:15 +0800

    统一选区圆角

--- packages/canvas/src/lib/TheCanvasSelectionArea.vue
+++ packages/canvas/src/lib/TheCanvasSelectionArea.vue
@@ -6,8 +6,9 @@
       height: `${selectionArea.size.h}px`,
       transform: `translate(${selectionArea.position.x}px,${selectionArea.position.y}px)`,
       borderWidth: `${1 / canvasBase.scale}px`,
+      borderRadius: `${3 / canvasBase.scale}px`,
     }"
-    class="border border-blue-600 bg-blue-600/10 fixed left-0 top-0 pointer-events-none rounded-sm origin-top-left z-50"
+    class="border border-blue-600 bg-blue-600/10 fixed left-0 top-0 pointer-events-none origin-top-left z-50"
   />
 </template>
 
