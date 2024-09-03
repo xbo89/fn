6f8470efbe72e36e938d8c3375c08e4a6cff46c5
happy wang <mail@wangwenbo.me>
2024-09-03 17:45:14 +0800

    补充测试结构

--- packages/dragger/src/playground.vue
+++ packages/dragger/src/playground.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="">
-    <DraggerPlugin />
+  <div class="w-[800px] h-[800px] bg-slate-200 pt-1 mx-auto flex justify-center items-center">
+    <div class=" relative w-[600px] h-[600px] bg-slate-300 overflow-hidden border border-blue-600">
+      <DraggerPlugin />
+    </div>
   </div>
 </template>
 
